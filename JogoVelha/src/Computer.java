f5ee2b0189e03ac1fe830fbd68e68c222979242a
Matheus Marques <matthlm@outlook.com>
2018-05-14 15:47:33 -0300

    Add files via upload

---
+++ JogoVelha/src/Computer.java
@@ -0,0 +1,38 @@
+
+import java.util.Random;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class Computer extends Player{
+    
+    public Computer(int player){
+        super(player);
+        System.out.println("Player 'Computer' created");
+    }
+    
+    @Override
+    public void play(Board board){  
+        Try(board);
+        board.setPosition(attempt, player);
+    }
+    
+    @Override
+    public void Try(Board board){
+        
+        Random rand = new Random();
+                
+        do{
+            attempt[0] = rand.nextInt(3); 
+            attempt[1] = rand.nextInt(3);
+            
+        }while(!checkTry(attempt, board));
+    }
+}
